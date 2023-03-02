c8d420b8f0288c3bdb5663e3e735a98570439378
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2023-03-02 22:37:22 +0100

    Some fun with logging in laravel

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        die('sfdsfdsfs');
+        Log::warning('There is a warning');
 
+        Log::error(false);
+
+        Log::notice(500);
+
+        $person = '{"Peter":35, "John":37, "Yinka":43}';
+
+        $data = json_decode($person, true);
+
+        info($data);
+
+        \Log::stack(['single', 'daily'])->info('registeration successful!');
+
+        die('sfdsfdsfs');
 
         //return view('user.profile', ['user' => User::findOrFail($id)]);
     }
